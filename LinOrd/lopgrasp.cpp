fb7c6e8c9758b591bd9bb54ad42d5305e16fed02
Carlos Oliveira <coliveira@gmail.com>
2009-11-17 15:28:44 -0500

    store statistics to be printed later clean up existing functions

--- LinOrd/lopgrasp.cpp
+++ LinOrd/lopgrasp.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -25,7 +26,7 @@ class GraspData {
 public:
 
     void  ConstructSolution(int* pSol);
-    bool InputInstance(const int argc, const char* datafile, int& problem_size);
+    bool InputInstance(const int argc, char **argv, int& problem_size);
     double GetSolutionValue (int* permutation);
     void printv();
     bool AllocData(int problem_size);
@@ -69,24 +70,104 @@ struct EliteEntry {
     }
 };
 
+int instanceError(const char *f) { printf("error reading instance %s\n", f); return 1; }
+int usage(const char *p) { printf("usage: %s <instanceFile>\n", p); return 0; }
+int failFile(const char *f) { printf("could not open file %s\n", f); return 0; }
+int failMem() { printf("error allocating memory\n"); return 1; }
+
+#ifdef NDEBUG
+#define DODBG(x) 
+#else
+#define DODBG(x) x
+#endif
+
+void printfRes0(double perc, double objective, double t) {
+    DODBG(printf("%.2lf%% %.0lf %ld ", perc, objective, t); fflush(stdout);)
+}
+
+void printfRes1(double o, double t1, int n, double perc) {
+    DODBG(printf("%.0lf %.2lf %d (imp %.2lf) ", o, t1, n, perc);)
+}
+
+void printfRes2(double o, double t2, double perc) {
+    DODBG(printf("%.0lf %.2lf (imp %.2lf)", o, t2, perc);)
+}
+
+void checkRes(int num_iterations, double objective, double bestValue) {
+DODBG(
+    if (num_iterations == 10) assert(objective == 3391268);
+    if (num_iterations == 10) assert(bestValue == 3391268);
+)
+}
+
+template<class T>bool allocVec(T *&v, int n) { v = new T[n]; return v != 0; }
+#define ALLOC_OR_RET(x, n) if (!allocVec(x, n)) return false;
+
+bool allocMemory(int n, int *&current_solution, int *&best_solution, int *&elite_start,
+                 EliteEntry ** &elite_set)
+{
+    ALLOC_OR_RET(current_solution, n);
+    ALLOC_OR_RET(best_solution, n);
+    ALLOC_OR_RET(elite_start, n);
+    ALLOC_OR_RET(elite_set, ELITE_SIZE);
+
+    for (int i=0; i<ELITE_SIZE; ++i) 
+        if (!(elite_set[i] = new EliteEntry(n))) return false;
+    return true;
+}
+
+// store temporary results, so we don't spend time printing them
+struct Results {
+    double obj1;
+    long time1;
+    double obj2;
+    double time2;
+    int n;
+    double perc1;
+    double obj3;
+    long time3;
+    double perc2;
+};
+
+
+void printRes(const vector<Results> &resultList)
+{
+#ifdef NDEBUG
+    FILE *f = fopen("..\\output.txt", "w");
+    int it_num=0;
+    for (vector<Results>::const_iterator i=resultList.begin(); i!=resultList.end(); i++, it_num++)
+    {
+        fprintf(f, "%.0lf %ld ", i->obj1, i->time1); fflush(stdout);
+        fprintf(f, "%.0lf %.2lf %d (imp %.2lf) ", i->obj2, i->time2, i->n, i->perc1); fflush(stdout);
+        if (it_num > ELITE_SIZE)
+        {
+            if (i->obj3 > i->obj2)
+                fprintf(f, "%.0lf %.2lf (imp %.2lf)", i->obj3, (double)i->time3, i->perc2);
+        }
+        fprintf(f, "\n");
+    }
+    fclose(f);
+#endif
+}
+
 int main(int argc, char** argv)
 {
-    srand (1); // (unsigned)time(NULL));
+   if (argc != 2) return usage(argv[0]);
 
+    srand (1); // (unsigned)time(NULL));
+    clock_t startTime = clock();
     int problem_size = 0;
     const char *fileName = argv[1];
-    if (g_GRASPData.InputInstance(argc, fileName, problem_size) == false ) 
-    {
-        printf("error reading instance %s\n", fileName);
-        return 1;
-    }
+    
+    if (!g_GRASPData.InputInstance(argc, argv, problem_size)) 
+        return instanceError(fileName);
 
-    int *current_solution = new int [problem_size];
-    int *best_solution = new int [problem_size];
-    int *elite_start = new int [problem_size];
+    int *current_solution, *best_solution,*elite_start;
+    EliteEntry **elite_set;
+    if (!allocMemory(problem_size, current_solution, best_solution, elite_start, elite_set)) 
+        return failMem();
 
-    EliteEntry **elite_set = new EliteEntry*[ELITE_SIZE];
-    for (int i=0; i<ELITE_SIZE; ++i) elite_set[i] = new EliteEntry(problem_size);
+    vector<Results> resultList;
 
     clock_t total_const_time=0, total_ls_time=0;
     int num_ls_iter = 0;
@@ -100,7 +181,10 @@ int main(int argc, char** argv)
         total_const_time += ct2-ct1;
 
         double delta, objective = g_GRASPData.GetSolutionValue(current_solution);
-        printf("%.0lf %ld ", objective, ct2-ct1); fflush(stdout);
+        Results res;
+        res.obj1 = objective; res.time1 = ct2-ct1;
+        double percCompleted = 100*(num_iterations/(double)nItLimit);
+        printfRes0(percCompleted, objective, ct2-ct1);
 
         // appy local search
         clock_t lst1 = clock();
@@ -111,9 +195,11 @@ int main(int argc, char** argv)
         clock_t lst2 = clock();
         total_ls_time += lst2 - lst1;
         num_ls_iter += n;
-        printf("%.0lf %.2lf %d (imp %.2lf) ", 
-            objective, (double)(lst2-lst1)/n, n, 100*(objective-orig_objective)/orig_objective); fflush(stdout);
 
+        // save info
+        res.obj2 = objective; res.time2 = (double)(lst2-lst1)/n; res.n = n;
+        res.perc1 = 100*(objective-orig_objective)/orig_objective;
+        printfRes1(res.obj2, res.time2, n, res.perc1);
 
         if (num_iterations > ELITE_SIZE) { // run path relinking
             clock_t prt1 = clock();
@@ -133,16 +219,17 @@ int main(int argc, char** argv)
             clock_t prt2 = clock();
             objective = g_GRASPData.GetSolutionValue(elite_start);
 
+            res.obj3 = objective;
             if (objective > orig_objective) {
                 for (int i=0; i<problem_size; ++i) current_solution[i] = elite_start[i];
-                printf("%.0lf %.2lf (imp %.2lf)", 
-                    objective, (double)(prt2-prt1), 100*(objective-orig_objective)/orig_objective);
+                res.time3 = prt2-prt1; res.perc2 = 100*(objective-orig_objective)/orig_objective;
+                printfRes2(objective, res.time3, res.perc2);
             } else {
                 objective = orig_objective;
             }
         }
 
-        printf("\n");
+        DODBG(printf("\n"));
 
         int pos=0; // find the first position where a solution can be inserted
         for (; pos<ELITE_SIZE && elite_set[pos]->cost > objective;)
@@ -161,18 +248,19 @@ int main(int argc, char** argv)
         if (objective > bestValue) {
             for (int i=0; i<problem_size; i++) best_solution[i] = current_solution[i];
             bestValue = objective;
-        }
 
-        if (num_iterations % 10 == 0)
-        {
-            if (num_iterations == 10) assert(objective == 3391268);
-            if (num_iterations == 10) assert(bestValue == 3391268);
-            printf("iter: %d best %d constr_time: %.2lf ls_time: %.2lf\n",
+            clock_t currentTime = clock();
+            printf("iter: %d best %d constr_time: %.2lf ls_time: %.2lf %ld\n",
                 num_iterations, (int)bestValue,
                 (double)total_const_time/num_iterations,
-                (double)total_ls_time/num_ls_iter);
+                (double)total_ls_time/num_ls_iter, currentTime - startTime);
         }
+
+        checkRes(num_iterations, objective, bestValue);
+        resultList.push_back(res);
     }
+
+    printRes(resultList);
 
     printf("\nFinal Permutation:\n");
     for (int i=0; i<problem_size; i++)
@@ -315,16 +403,20 @@ bool GraspData::AllocData(int problem_size)
     return true;
 }
 
+template<class T>void vecDelete(T *v) { if (v) delete[] v; }
+template<class T>void mvecDelete(T *v, int n) { 
+    if (v) for (int i=0; i<n; ++i) vecDelete(v[i]);
+    vecDelete(v);
+}
+
 GraspData::~GraspData()
 {
-    if (greedy_value) delete[] greedy_value;
-    if (copy_of_values) delete[] copy_of_values;
-    if (greedy_order) delete[] greedy_order;
-    if (copy_of_order) delete[] copy_of_order;
-    if (costs)  for (int i=0; i<length; ++i) delete[] costs[i];
-    if (costs)  delete[] costs;
-    if (ls_entries) for (int i=0; i<length; ++i) delete[] ls_entries[i];
-    if (ls_entries) delete[] ls_entries;
+    vecDelete(greedy_value);
+    vecDelete(copy_of_values);
+    vecDelete(greedy_order);
+    vecDelete(copy_of_order);
+    mvecDelete (costs, length);
+    mvecDelete (ls_entries, length);
 }
 
 bool GraspData::ReadInstanceValues(ifstream &file)
@@ -350,20 +442,12 @@ bool GraspData::ReadInstanceValues(ifstream &file)
     return true;
 }
 
-bool GraspData::InputInstance(const int argc, const char* datafile, int& problem_size)
+bool GraspData::InputInstance(const int argc, char **argv, int& problem_size)
 {
-    if (argc != 2) 
-    {
-        printf("usage: %s <instanceFile>\n");
-        return false;
-    }
-
-    ifstream file(datafile, ifstream::in);
-    if (file.fail()) 
-    {
-        printf("could not open file %s\n", datafile);
-        return false;
-    }
+    ifstream file(argv[1], ifstream::in);
+    
+    if (file.fail()) return failFile(argv[1]);
+    
     const int buffer_size = 1024*10;
     char str[buffer_size];
 
@@ -374,7 +458,7 @@ bool GraspData::InputInstance(const int argc, const char* datafile, int& problem
     printf("  Max #Iterations: %d\n", nItLimit);
     printf("  Alpha: %lf\n", ALPHA);
     printf("Problem\n");
-    printf("  Filename: %s\n", datafile);
+    printf("  Filename: %s\n", argv[1]);
     printf("  Title: %s\n", str);
 
     ReadInstanceValues(file);
